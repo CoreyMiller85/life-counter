a0f1c7c5493ba375a44994ccf8a0a3073c220170
Corey Miller <coreymiller454@gmail.com>
2021-07-02 00:01:30 -0400

    add click event and animation to settings button

--- src/components/GameButton.js
+++ src/components/GameButton.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCogs } from "@fortawesome/free-solid-svg-icons";
 import './GameButton.css'
 
 
 function GameButton(props) {
+  const [clicked, setClicked] =useState(false)
+
+  function handleGameButtonClick(e) {
+    setClicked(!clicked)
+  }
+
   return (
-    <div className="game-button">
-      <div className="btn">
+    <div className={clicked ? "game-button clicked" : "game-button"}>
+      <div className='btn' onClick={(e) => handleGameButtonClick(e)}>
         <FontAwesomeIcon icon={faCogs} className="cogs"/>
       </div>
     </div>
